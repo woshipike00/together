8c91d38ebadeaef783f5f485ffb781bc5f1a7db0
Yu Dongliang <yudl.nju@gmail.com>
2013-04-05 22:12:17 +0800

    introduce list_event

--- src/together/connectivity/JsonHandler.java
+++ src/together/connectivity/JsonHandler.java
@@ -54,10 +54,37 @@ public class JsonHandler {
 //			String obj2 = object1.getString("star");
 			JSONObject obj = new JSONObject(array.getJSONObject(i).getString("star"));
 			msg = new EventMsg();
-			msg.setEvent(obj.getString("event"));
-			msg.setId(obj.getString("id"));
-			msg.setName(obj.getString("name"));
-			msg.setTime(obj.getString("time"));
+			//TODO 获取正确信息
+//			msg.setEvent(obj.getString("event"));
+//			msg.setId(obj.getString("id"));
+//			msg.setName(obj.getString("name"));
+//			msg.setTime(obj.getString("time"));
+			msgs.add(msg);
+		}
+		return msgs;
+	}
+	
+	public List<EventMsg> getMessages(String json, String type) throws JSONException {
+		msgs = new ArrayList<EventMsg>();
+		JSONObject object = new JSONObject(json);
+		JSONArray array = object.getJSONArray(type);
+		int length = array.length();
+		for (int i = 0; i < length; i++) {
+//			JSONObject object1 = array.getJSONObject(i);
+//			String obj2 = object1.getString("star");
+			JSONObject obj = array.getJSONObject(i);
+			msg = new EventMsg();
+			msg.setEid(obj.getString("eid"));
+			msg.setEname(obj.getString("place"));
+			msg.setUid(obj.getString("uid"));
+			msg.setType(obj.getString("type"));
+			msg.setDescription(obj.getString("description"));
+			msg.setLongitude(obj.getString("longitude"));
+			msg.setLatitude(obj.getString("latitude"));
+			msg.setStartDate(obj.getString("startDate"));
+			msg.setStartTime(obj.getString("startTime"));
+			msg.setEndDate(obj.getString("endDate"));
+			msg.setEndTime(obj.getString("endTime"));
 			msgs.add(msg);
 		}
 		return msgs;
--- src/together/models/EventMsg.java
+++ src/together/models/EventMsg.java
@@ -7,62 +7,155 @@
 package together.models;
 
 public class EventMsg {
-	private String id;
-	private String username;
-	private String event;
-	private String url;
-	private String large_url;
-	private String time;
+	private String eid;
+	private String place;
+	private String uid;
+	private String type;
+	private String description;
+	private String longitude;
+	private String latitude;
+	private String startDate;
+	private String StartTime;
+	private String endDate;
+	private String endTime;
 	
-	public String getTime() {
-		return time;
+	public String getEid() {
+		return eid;
 	}
 
-	public void setTime(String time) {
-		this.time = time;
+	public void setEid(String eid) {
+		this.eid = eid;
 	}
 
-	public String getId() {
-		return id;
+	public String getPlace() {
+		return place;
 	}
 
-	public void setId(String id) {
-		this.id = id;
+	public void setEname(String ename) {
+		this.place = ename;
 	}
 
-	public String getName() {
-		return username;
+	public String getUid() {
+		return uid;
 	}
 
-	public void setName(String name) {
-		this.username = name;
+	public void setUid(String uid) {
+		this.uid = uid;
 	}
 
-	public String getEvent() {
-		return event;
+	public String getType() {
+		return type;
 	}
 
-	public void setEvent(String description) {
-		this.event = description;
+	public void setType(String type) {
+		this.type = type;
 	}
 
-	public String getUrl() {
-		return url;
+	public String getLongitude() {
+		return longitude;
 	}
 
-	public void setUrl(String url) {
-		this.url = url;
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
 	}
 
-	public String getLargeUrl() {
-		return large_url;
+	public String getLatitude() {
+		return latitude;
 	}
 
-	public void setLargeUrl(String url) {
-		this.large_url = url;
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
 	}
+
+	public String getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+
+	public String getStartTime() {
+		return StartTime;
+	}
+
+	public void setStartTime(String startTime) {
+		StartTime = startTime;
+	}
+
+	public String getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+
+	public String getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(String endTime) {
+		this.endTime = endTime;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+//
+//	public String getTime() {
+//		return latitude;
+//	}
+//
+//	public void setTime(String time) {
+//		this.latitude = time;
+//	}
+//
+//	public String getId() {
+//		return eid;
+//	}
+//
+//	public void setId(String id) {
+//		this.eid = id;
+//	}
+//
+//	public String getName() {
+//		return ename;
+//	}
+//
+//	public void setName(String name) {
+//		this.ename = name;
+//	}
+//
+//	public String getEvent() {
+//		return uid;
+//	}
+//
+//	public void setEvent(String description) {
+//		this.uid = description;
+//	}
+//
+//	public String getUrl() {
+//		return type;
+//	}
+//
+//	public void setUrl(String url) {
+//		this.type = url;
+//	}
+//
+//	public String getLargeUrl() {
+//		return longitude;
+//	}
+//
+//	public void setLargeUrl(String url) {
+//		this.longitude = url;
+//	}
 
 	public void print() {
-		System.out.println(id + "," + username);
+		System.out.println(eid + "," + place);
 	}
 }
